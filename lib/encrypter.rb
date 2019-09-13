245f469a42dc1a6a79963f78682aabedeaaa7ba2
John Travers <johnk.travers@gmail.com>
2019-09-13 11:55:21 -0600

    Add shift_message method and test

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,3 +1,4 @@
+require 'date'
 require './lib/shiftable'
 
 class Encrypter
@@ -5,10 +6,27 @@ class Encrypter
 
   attr_reader :message, :key, :date
 
-  def initialize(message, key = "12345", date = "130919")
+  def initialize(message, key = '12345', date = Date.today.strftime('%d%m%y'))
     @message = message
     @key = key
     @date = date
+    @alphabet = ('a'..'z').to_a << ' '
+  end
+
+  def shift_message
+    shifts = get_shifts
+    encrypted_message = @message.downcase.split('')
+
+    encrypted_message.each_with_index do |char, index|
+      if @alphabet.include?(char)
+        alpha_index = @alphabet.index(char)
+        shifted_alpha = @alphabet.rotate(shifts[index % 4])
+        encrypted_message[index] = shifted_alpha[alpha_index]
+        require "pry"; binding.pry
+      end
+    end
+
+    encrypted_message.join('')
   end
 
 end
--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -9,8 +9,8 @@ require './lib/encrypter'
 class EncrypterTest < Minitest::Test
 
   def setup
-    @encrypter_1 = Encrypter.new("Hello, world!", "56309", "250684")
-    @encrypter_2 = Encrypter.new("Hello, world!")
+    @encrypter_1 = Encrypter.new('HEllO woRLd eNd', '08304', '291018')
+    @encrypter_2 = Encrypter.new('Hello, world!')
   end
 
   def test_it_exists
@@ -19,13 +19,18 @@ class EncrypterTest < Minitest::Test
   end
 
   def test_initialize
-    assert_equal "Hello, world!", @encrypter_1.message
-    assert_equal "56309", @encrypter_1.key
-    assert_equal "250684", @encrypter_1.date
+    assert_equal 'HEllO woRLd eNd', @encrypter_1.message
+    assert_equal '08304', @encrypter_1.key
+    assert_equal '291018', @encrypter_1.date
 
-    assert_equal "Hello, world!", @encrypter_2.message
-    assert_equal "12345", @encrypter_2.key
-    assert_equal "130919", @encrypter_2.date
+    assert_equal 'Hello, world!', @encrypter_2.message
+    assert_equal '12345', @encrypter_2.key
+    assert_equal '130919', @encrypter_2.date
+  end
+
+  def test_shift_message
+    # assert_equal 'vjqtbeaweqihssi', @encrypter_1.shift_message
+    assert_equal 'xfydd,modsyw!', @encrypter_2.shift_message
   end
 
 end

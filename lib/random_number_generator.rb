acedf8f908c61b53266bf248ef9c7827c2be67aa
John Travers <johnk.travers@gmail.com>
2019-09-13 12:37:56 -0600

    Change get_number from instance method to class method

--- lib/random_number_generator.rb
+++ lib/random_number_generator.rb
@@ -1,6 +1,7 @@
 class RandomNumberGenerator
 
-  def get_number
+  # Generate number for any number of digits
+  def self.get_number
     rand(100000).to_s.rjust(5, '0')
   end
 
--- test/random_number_generator_test.rb
+++ test/random_number_generator_test.rb
@@ -7,18 +7,17 @@ require './lib/random_number_generator'
 
 class RandomNumberGeneratorTest < Minitest::Test
 
-  def setup
-    @rand_num_gen = RandomNumberGenerator.new
-  end
-
   def test_it_exists
-    assert_instance_of RandomNumberGenerator, @rand_num_gen
+    rand_num_gen = RandomNumberGenerator.new
+
+    assert_instance_of RandomNumberGenerator, rand_num_gen
   end
 
   def test_get_number
-    assert_equal true, @rand_num_gen.get_number.to_i > 0
-    assert_equal true, @rand_num_gen.get_number.to_i <= 99999
-    assert_equal true, @rand_num_gen.get_number.length == 5
+    number = RandomNumberGenerator.get_number
+
+    assert_equal true, number.to_i > 0 && number.to_i <= 99999
+    assert_equal true, number.length == 5
   end
 
 end

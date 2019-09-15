8a8c78bf7bda64bf61c9ebd2210e9b6bb66fb319
John Travers <johnk.travers@gmail.com>
2019-09-14 21:24:57 -0600

    Add test for encrypt method with given message and key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -16,13 +16,24 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_encrypt
+  def test_encrypt_with_message_key_and_date
     expected = {
       encryption: 'keder ohulw',
       key: '02715',
       date: '040895'
     }
     assert_equal expected, @enigma.encrypt('hello world', '02715', '040895')
+  end
+
+  def test_encrypt_with_message_and_key
+    Date.expects(:today).returns(Date.new(1985, 11, 16))
+
+    expected = {
+      encryption: 'bjqtngdeublaktx.',
+      key: '02715',
+      date: '161185'
+    }
+    assert_equal expected, @enigma.encrypt('Why hello there.', '02715')
   end
 
   def test_encrypt_with_message_only

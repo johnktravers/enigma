6daf2fec14dce167fc9c7de69e8a8ff65e985fab
John Travers <johnk.travers@gmail.com>
2019-09-16 16:43:44 -0600

    Change shift_message method to take a default parameter

--- lib/shiftable.rb
+++ lib/shiftable.rb
@@ -9,7 +9,6 @@ module Shiftable
 
   def get_offsets(date = @date)
     digits = (date.to_i ** 2).to_s
-
     [ digits[-4].to_i,
       digits[-3].to_i,
       digits[-2].to_i,
@@ -84,9 +83,9 @@ module Shiftable
     aligned[0] + aligned[1][1] + aligned[2][1] + aligned[3][1]
   end
 
-  def shift_message(shifts)
+  def shift_message(text = @message, shifts)
     alphabet = ('a'..'z').to_a << ' '
-    shifted_message = @message.downcase.split('')
+    shifted_message = text.downcase.split('')
 
     shifted_message.each_with_index do |char, index|
       if alphabet.include?(char)

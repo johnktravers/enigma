9b40f28ecf3433bfa22f0179d3efcff3f9874653
John Travers <johnk.travers@gmail.com>
2019-09-17 21:45:29 -0600

    Reorder methods for readability

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -12,12 +12,12 @@ class Cracker
   end
 
   def crack_ciphertext
-    shifts = get_crack_shifts(ciphertext)
-    shift_text(ciphertext, shifts)
+    shifts = get_crack_shifts(@ciphertext)
+    shift_text(@ciphertext, shifts)
   end
 
   def crack_key
-    keys = get_crack_keys(ciphertext, date)
+    keys = get_crack_keys(@ciphertext, date)
     create_crack_key(keys)
   end
 
@@ -27,10 +27,10 @@ class Cracker
   def get_crack_shifts(ciphertext)
     split = ciphertext.split('')
     shifts = []
-    shifts[0] = @alphabet.index(' ') - @alphabet.index(split[-4])
-    shifts[1] = @alphabet.index('e') - @alphabet.index(split[-3])
-    shifts[2] = @alphabet.index('n') - @alphabet.index(split[-2])
-    shifts[3] = @alphabet.index('d') - @alphabet.index(split[-1])
+    shifts[0] = (@alphabet.index(' ') - @alphabet.index(split[-4])) % 27
+    shifts[1] = (@alphabet.index('e') - @alphabet.index(split[-3])) % 27
+    shifts[2] = (@alphabet.index('n') - @alphabet.index(split[-2])) % 27
+    shifts[3] = (@alphabet.index('d') - @alphabet.index(split[-1])) % 27
     shifts.rotate!(4 - (split.length % 4))
   end
 
@@ -39,15 +39,35 @@ class Cracker
     shifts = get_crack_shifts(ciphertext)
     offsets = get_offsets(date)
     shifts.each_with_index do |shift, index|
-      keys.push( -1 * shift - offsets[index])
+      keys.push((-1 * shift - offsets[index]) % 27)
     end
     keys
   end
 
+  def create_crack_key(keys)
+    filtered = filter_key_combos(keys)
+
+    if filtered.length == 1
+      filtered.flatten!
+      return filtered[0] + filtered[1][1] + filtered[2][1] + filtered[3][1]
+    elsif filtered.length > 1
+      filtered.each do |key_arr|
+        keys_attempt = key_arr.map { |key| key.to_i }
+        if shift_text(@ciphertext, get_shifts(keys_attempt, @date))
+          return key_arr[0] + key_arr[1][1] + key_arr[2][1] + key_arr[3][1]
+        end
+      end
+    else
+      '(No key found)'
+    end
+  end
+
+
+  #------------Helper Helper Methods------------#
+
   def possible_keys(keys)
-    possible = []
+    possible = Array.new(4) { Array.new }
     keys.each_with_index do |key, index|
-      possible[index] = []
       num = key % 27
       until num >= 100
         possible[index].push(num.to_s.rjust(2, '0'))
@@ -67,25 +87,6 @@ class Cracker
     combos.find_all do |combo|
       combo[0][1] == combo[1][0] && combo[1][1] == combo[2][0] && combo[2][1] == combo[3][0]
     end
-  end
-
-  def create_crack_key(keys)
-    filtered = filter_key_combos(keys)
-
-    if filtered.length == 1
-      filtered.flatten!
-      return filtered[0] + filtered[1][1] + filtered[2][1] + filtered[3][1]
-    elsif filtered.length > 1
-      filtered.each do |key_arr|
-        keys_attempt = key_arr.map { |key| key.to_i }
-        if shift_text(@ciphertext, get_shifts(keys_attempt, @date))
-          return key_arr[0] + key_arr[1][1] + key_arr[2][1] + key_arr[3][1]
-        end
-      end
-    else
-      '(No key found)'
-    end
-
   end
 
 end

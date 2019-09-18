b784384fa884919d5bb158a9e0794f3b4667db5e
John Travers <johnk.travers@gmail.com>
2019-09-17 20:54:56 -0600

    Add assertion statements for non-default inputs of methods

--- test/shiftable_test.rb
+++ test/shiftable_test.rb
@@ -13,14 +13,20 @@ class ShiftableTest < Minitest::Test
 
   def test_get_keys
     assert_equal [8, 83, 30, 4], @encrypter.get_keys
+    assert_equal [26, 61, 14, 49], @encrypter.get_keys('26149')
   end
 
   def test_get_offsets
     assert_equal [6, 3, 2, 4], @encrypter.get_offsets
+    assert_equal [6, 4, 0, 9], @encrypter.get_offsets('090597')
   end
 
   def test_get_shifts
-    assert_equal [14, 86, 32, 8], @encrypter.get_shifts
+    assert_equal [14, 5, 5, 8], @encrypter.get_shifts
+    assert_equal [5, 10, 16, 26], @encrypter.get_shifts([26, 61, 14, 49])
+
+    expected = [5, 11, 14, 4]
+    assert_equal expected, @encrypter.get_shifts([26, 61, 14, 49], '090597')
   end
 
   def test_shift_text
